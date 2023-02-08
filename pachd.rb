3b19566e2835cbeaa865a920966b9502395024b4
acohen4 <alonco124@gmail.com>
2023-02-08 11:33:26 -0500

    wip

--- pachd.rb
+++ pachd.rb
@@ -4,9 +4,10 @@ class Pachd < Formula
   homepage "github.com/pachyderm/pachyderm"
   version "v2.4.3"
   url "https://github.com/pachyderm/pachyderm/blob/master/etc/reset.py"
-    
+  sha256 "c9ebb13a579708bcf236fa01ddcfb66b486a12ca0df523584ba5377d96c40a8b"
+  
   def install
-    #bin.install buildpath/"pachd"
+    bin.install buildpath/"pachd"
   end
 
   service do
