34992b20b02c511e1df134b3b37b99af7e80d186
acohen4 <alonco124@gmail.com>
2023-02-08 11:17:27 -0500

    wip

--- pachd.rb
+++ pachd.rb
@@ -3,12 +3,11 @@ require "formula"
 class Pachd < Formula
   homepage "github.com/pachyderm/pachyderm"
   version "v2.4.2"
-
-  url "https://github.com/pachyderm/pachyderm/releases/download/v2.4.2/pachd"
-  sha256 "0320ef9f12efd4995d6e859b7ec5b0be2a54168dcede85ff07bc616525b66554"
+  # url "https://github.com/pachyderm/pachyderm/releases/download/v2.4.2/pachd"
+  # sha256 "0320ef9f12efd4995d6e859b7ec5b0be2a54168dcede85ff07bc616525b66554"
     
   def install
-    bin.install buildpath/"pachd"
+    #bin.install buildpath/"pachd"
   end
 
   service do
