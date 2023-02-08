0b41b3207cb71174095854b869fcc1f044e342f4
acohen4 <alonco124@gmail.com>
2023-02-08 11:29:01 -0500

    wip

--- pachd.rb
+++ pachd.rb
@@ -3,7 +3,8 @@ require "formula"
 class Pachd < Formula
   homepage "github.com/pachyderm/pachyderm"
   version "v2.4.3"
-  url "https://github.com/acohen4/homebrew-lonny-tap"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v2.4.5/pachctl_2.4.5_darwin_amd64.zip"
+  sha256 "64350fc7a1ac1afea2fd296e252ff16e0fe1c51a7a12fbdf6a8da1f685a39f59"
     
   def install
     #bin.install buildpath/"pachd"
