d0baa63fcee35837b8f9da39ed805409a5ad6883
acohen4 <alonco124@gmail.com>
2023-02-08 17:10:47 -0500

    wip

--- pachd.rb
+++ pachd.rb
@@ -3,7 +3,8 @@ require "formula"
 class Pachd < Formula
   homepage "github.com/pachyderm/pachyderm"
   version "v2.4.3"
-  url "https://github.com/acohen4/pach-suite/blob/master/pach-suite.sh"
+  url "https://github.com/acohen4/pach-suite/blob/master/pach-suite"
+  sha256 "c92c6e5ab133aa12a34b0ac929fede7d89af198ad2572c9a4229d757ff574c2e"
 
   depends_on "postgresql"
   depends_on "etcd"
