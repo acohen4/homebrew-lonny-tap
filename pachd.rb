2b51f99c4bacc1c0482d0ef3862e609cef39d2bd
acohen4 <alonco124@gmail.com>
2023-02-09 09:32:18 -0500

    wip

--- pachd.rb
+++ pachd.rb
@@ -3,9 +3,9 @@ require "language/go"
 require 'erb'
 
 class Pachd < Formula
-  homepage "github.com/pachyderm/pachyderm"
+  homepage "https://github.com/acohen4/pach-suite"
   version "v2.4.3"
-  url "https://github.com/acohen4/pach-suite/blob/master/pach-suite?raw=true"
+  url "https://github.com/acohen4/pach-suite/blob/main/bin/pach.zip"
   sha256 "1c60d4118fc975d83bc6ca5718c08fae592113d33636ae991ce412c687684a74"
 
   depends_on "postgresql"
