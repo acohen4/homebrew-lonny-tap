2941007c9d19ad9591de6a460103c289fb0ceb5a
acohen4 <alonco124@gmail.com>
2023-02-09 09:46:42 -0500

    wip

--- pachd.rb
+++ pachd.rb
@@ -6,7 +6,7 @@ class Pachd < Formula
   homepage "https://github.com/acohen4/pach-suite"
   version "v2.4.3"
   url "https://raw.githubusercontent.com/acohen4/pach-suite/main/bin/pach/pach-suite"
-  sha256 "66dcb7e8ded2b1f51b647381846f948d821c17951dd708c88801706e9270b12e"
+  sha256 "04864c396b4c3d7822ff18cbf9c35ede359515ac6bf727bb8657231943091bfc"
 
   depends_on "postgresql"
   depends_on "etcd"
