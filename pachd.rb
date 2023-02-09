80e6db9889ca2bdbb25810fc78303ddc50b0f54c
acohen4 <alonco124@gmail.com>
2023-02-09 09:46:01 -0500

    wip

--- pachd.rb
+++ pachd.rb
@@ -5,7 +5,7 @@ require 'erb'
 class Pachd < Formula
   homepage "https://github.com/acohen4/pach-suite"
   version "v2.4.3"
-  url "https://github.com/acohen4/pach-suite/blob/main/bin/pach.zip"
+  url "https://raw.githubusercontent.com/acohen4/pach-suite/main/bin/pach/pach-suite"
   sha256 "66dcb7e8ded2b1f51b647381846f948d821c17951dd708c88801706e9270b12e"
 
   depends_on "postgresql"
